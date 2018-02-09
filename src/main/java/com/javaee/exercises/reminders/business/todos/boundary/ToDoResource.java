858f7f0541b7db2a1d550af215ae2e2c1615d2d7
ldoustaly <luciendoustaly@icloud.com>
2018-02-09 17:26:31 +0100

    Add put, delete

--- src/main/java/com/javaee/exercises/reminders/business/todos/boundary/ToDoResource.java
+++ src/main/java/com/javaee/exercises/reminders/business/todos/boundary/ToDoResource.java
@@ -6,24 +6,38 @@
 package com.javaee.exercises.reminders.business.todos.boundary;
 
 import com.javaee.exercises.reminders.business.todos.entity.ToDo;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import static javax.ws.rs.HttpMethod.PUT;
+import javax.ws.rs.PUT;
 
 /**
  *
  * @author ldoustaly
  */
-
 public class ToDoResource {
+
     long id;
     ToDoManager manager;
 
     public ToDoResource(long id, ToDoManager manager) {
         this.id = id;
         this.manager = manager;
-    }           
-    
+    }
+
     @GET
-    public ToDo find() {   
+    public ToDo find() {
         return manager.findById(id);
+    }
+
+    @PUT
+    public ToDo save(ToDo todo) {
+        todo.setId(id);
+        return manager.save(todo);
+    }
+
+    @DELETE
+    public void delete() {
+        manager.delete(id);
     }
 }
