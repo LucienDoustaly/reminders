8ed5ba4d3614ea4a641cad39e27097fcba98be93
ldoustaly <luciendoustaly@icloud.com>
2018-02-15 12:22:45 +0100

    API OK

--- src/main/java/com/javaee/exercises/reminders/business/todos/boundary/TodosResource.java
+++ src/main/java/com/javaee/exercises/reminders/business/todos/boundary/TodosResource.java
@@ -6,20 +6,25 @@
 package com.javaee.exercises.reminders.business.todos.boundary;
 
 import com.javaee.exercises.reminders.business.todos.entity.ToDo;
+import java.net.URI;
 import java.util.List;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
+import javax.validation.Valid;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
 
 /**
  *
  * @author ldoustaly
  */
-
 @Stateless
 @Path("todos")
 @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
@@ -36,5 +41,13 @@ public class TodosResource {
     @Path("{id}")
     public ToDoResource find(@PathParam("id") long id) {
         return new ToDoResource(id, manager);
+    }
+
+    @POST
+    public Response save(@Valid ToDo todo, @Context UriInfo info) {
+        ToDo saved = manager.save(todo);
+        long id = saved.getId();
+        URI uri = info.getAbsolutePathBuilder().path("/" + id).build();
+        return Response.created(uri).build();
     }
 }
